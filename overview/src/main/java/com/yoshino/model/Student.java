3aa2a35f78204252a2ffd79c451508fd87b01504
wangxin <459765682@qq.com>
2022-04-18 01:25:15 +0800

    code check

--- overview/src/main/java/com/yoshino/model/Student.java
+++ overview/src/main/java/com/yoshino/model/Student.java
@@ -22,19 +22,27 @@ public class Student {
     public Student() {
     }
 
-    public Student(String firstName, String lastName, String email){
+    public Student(String firstName, String lastName, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
     }
 
-    public long getId() { return id; }
+    public long getId() {
+        return id;
+    }
 
-    public String getFirstName() { return firstName; }
+    public String getFirstName() {
+        return firstName;
+    }
 
-    public String getLastName() { return lastName; }
+    public String getLastName() {
+        return lastName;
+    }
 
-    public String getEmail() { return email; }
+    public String getEmail() {
+        return email;
+    }
 
     @Override
     public String toString() {
