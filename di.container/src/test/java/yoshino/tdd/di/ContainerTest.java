3b4e8452d22972465953bbf7ba68df29d56d8c1f
wangxin <459765682@qq.com>
2023-01-02 22:48:33 +0800

    di container: step 6 - testcase refactor - 抽象泛化(参数化覆盖范围，举个例子)

--- di.container/src/test/java/yoshino/tdd/di/ContainerTest.java
+++ di.container/src/test/java/yoshino/tdd/di/ContainerTest.java
@@ -2,11 +2,18 @@ package yoshino.tdd.di;
 
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Named;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -52,8 +59,10 @@ public class ContainerTest {
                 assertSame(exception.getInstance(), Dependency.class);
             }
 
-            @Test
-            public void should_throw_exception_if_exist_cyclic_dependencies() {
+            @ParameterizedTest
+            @MethodSource
+            public void should_throw_exception_if_exist_cyclic_dependencies(Class<? extends Component> component,
+                                                                            Class<? extends Dependency> dependency) {
                 config.bind(Component.class, ComponentWithDependencyInjectedConstructor.class);
                 config.bind(Dependency.class, DependencyDependedOnComponent.class);
 
@@ -62,6 +71,71 @@ public class ContainerTest {
                 assertTrue(exception.getDependencies().contains(Component.class));
                 assertTrue(exception.getDependencies().contains(Dependency.class));
             }
+
+            private static Stream<Arguments> should_throw_exception_if_exist_cyclic_dependencies() {
+                List<Arguments> result = new ArrayList<>();
+                for (Named<? extends Class<? extends Component>> component : List.of(
+                    Named.of("inject constructor", ComponentInjectConstructor.class),
+                    Named.of("inject field", ComponentInjectField.class),
+                    Named.of("inject method", ComponentInjectMethod.class))) {
+                    for (Named<? extends Class<? extends Dependency>> dependency : List.of(
+                        Named.of("inject constructor", CyclicDependencyInjectConstructor.class),
+                        Named.of("inject field", CyclicDependencyInjectField.class),
+                        Named.of("inject method", CyclicDependencyInjectMethod.class))) {
+                        result.add(Arguments.of(component, dependency));
+                    }
+                }
+                return result.stream();
+            }
+
+            class ComponentInjectConstructor implements Component {
+                private Dependency dependency;
+                @Inject
+                public ComponentInjectConstructor(Dependency dependency) {
+                    this.dependency = dependency;
+                }
+            }
+
+            class ComponentInjectField implements Component {
+                @Inject
+                private Dependency dependency;
+
+                public ComponentInjectField() {
+                }
+            }
+
+            class ComponentInjectMethod implements Component {
+                private Dependency dependency;
+
+                public ComponentInjectMethod() {
+                }
+                @Inject
+                public void install(Dependency dependency) {}
+            }
+
+            class CyclicDependencyInjectConstructor implements Dependency {
+                private Component component;
+                @Inject
+                public CyclicDependencyInjectConstructor(Component component) {
+                }
+            }
+
+            class CyclicDependencyInjectField implements Dependency {
+                @Inject
+                private Component component;
+
+                public CyclicDependencyInjectField() {
+                }
+            }
+
+            class CyclicDependencyInjectMethod implements Dependency {
+
+                public CyclicDependencyInjectMethod() {
+                }
+                @Inject
+                public void install(Component component) {}
+            }
+
 
             @Test
             public void should_throw_exception_if_exist_transitive_cyclic_dependencies() {
@@ -75,7 +149,6 @@ public class ContainerTest {
                 assertTrue(exception.getDependencies().contains(Dependency.class));
                 assertTrue(exception.getDependencies().contains(AnotherDependency.class));
             }
-
         }
 
     }
